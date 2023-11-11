4715695427b582317a7dbb224f704b261291eae4
Lilian <liliannalil347@gmail.com>
2023-11-11 18:19:06 +0100

    creation packages

--- src/module-info.java
+++ src/module-info.java
@@ -7,4 +7,13 @@
 module poo {
 	requires java.base;
 	requires junit;
+	
+	//exports
+	exports game;
+	exports characters;
+	exports items;
+	//exports hero;
+	//exports locations;
+	//exports quests;
+	exports tests;
 }
---
+++ src/tests/GameTest.java
@@ -0,0 +1,13 @@
+package tests;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import game.Game;
+
+public class GameTest {
+	@Test
+	public void testTrue() {
+		assertTrue(true);
+	}
+}
