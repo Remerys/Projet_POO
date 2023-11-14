6326c4a3a94a2800d67871effebffdbea60d94d3
PhiphiLeBoss12 <100560205+PhiphiLeBoss12@users.noreply.github.com>
2023-11-14 14:34:26 +0100

    Added constructor to Game

--- src/game/Game.java
+++ src/game/Game.java
@@ -1,5 +1,16 @@
 package game;
 
-public class Game {
+import java.util.ArrayList;
+import java.util.List;
 
+import hero.Hero;
+import locations.Location;
+
+public class Game {
+    Hero hero;
+    List<Location> locations = new ArrayList<Location>();
+
+    public Game() {
+        // Location.createLocations();
+    }
 }
