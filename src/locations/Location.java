fe77d1cbd605f9932855a3b4ef401fa440d58e7f
PhiphiLeBoss12 <100560205+PhiphiLeBoss12@users.noreply.github.com>
2023-11-14 15:49:22 +0100

    Added Location constructor and attributes

--- src/locations/Location.java
+++ src/locations/Location.java
@@ -1,5 +1,20 @@
 package locations;
 
+import java.util.HashMap;
+import java.util.List;
+
 public abstract class Location {
+    private final String NAME;
+    private final String DESCRIPTION;
+
+    private final HashMap<String, Exit> EXITS = new HashMap();
+
+    /**
+     * Creates a new location from its name and its description
+     */
+    public Location(String name, String description) {
+        this.NAME = name;
+        this.DESCRIPTION = description;
+    }
 
 }
