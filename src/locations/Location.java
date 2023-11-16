15bfdaf294bbfa4b37124fcbca891b50a4dc29b9
PhiphiLeBoss12 <100560205+PhiphiLeBoss12@users.noreply.github.com>
2023-11-16 16:42:07 +0100

    Changed code structure and added locations

--- src/locations/Location.java
+++ src/locations/Location.java
@@ -1,5 +1,6 @@
 package locations;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -7,7 +8,13 @@ public abstract class Location {
     private final String NAME;
     private final String DESCRIPTION;
 
-    private final HashMap<String, Exit> EXITS = new HashMap();
+    private HashMap<String, Exit> exits = new HashMap<String, Exit>();
+    private List<Character> characters = new ArrayList<Character>();
+
+
+    /* --------------------*
+     *    Constructors     *
+     * --------------------*/
 
     /**
      * Creates a new location from its name and its description
@@ -17,19 +24,24 @@ public abstract class Location {
         this.DESCRIPTION = description;
     }
 
+
+    /* --------------------*
+     *       Setters       *
+     * --------------------*/
+
     /**
      * Adds an exit to the location
      */
     public void addExit(String name, Exit exit) {
-        this.EXITS.put(name, exit);
+        this.exits.put(name, exit);
     }
 
     /**
      * Removes the exit from the location if it exists. Else throws an error.
      */
-    public void removeExit(Exit exit) throws Exception {
-        if (this.EXITS.containsValue(exit)){
-            this.EXITS.remove(exit);
+    public void removeExit(String exitName) throws Exception {
+        if (this.exits.containsKey(exitName)){
+            this.exits.remove(exitName);
         } 
         else {
             throw new Exception("removeExit : The exit doesn't exist !");
@@ -37,19 +49,49 @@ public abstract class Location {
     }
 
     /**
-     * Returns the new location associated to the string
+     * Adds a given character to the location
      */
-    public Location exitTo(String exit)  {
-        return EXITS.get(exit).exitTo(exit);
+    public void addCharacter(Character c) {
+        this.characters.add(c);
     }
 
+    /**
+     * Removes a given character from the location
+     */
+    public void removeCharacter(Character c) {
+        this.characters.remove(c);
+    }
+
+
+    /* --------------------*
+     *       Getters       *
+     * --------------------*/
+
+    /**
+     * Returns the name of the location
+     */
     public String getName() {
         return this.NAME;
     }
 
+    /**
+     * Returns the description of the location
+     */
     public String getDescription() {
         return this.DESCRIPTION;
     }
 
-    // TODO: Characters
+
+    /* --------------------*
+     *    Other Methods    *
+     * --------------------*/
+
+    /**
+     * Returns the new location associated to the string
+     */
+    public Location exitTo(String exit)  {
+        return this.exits.get(exit).exitTo(exit);
+    }
+
+    
 }
