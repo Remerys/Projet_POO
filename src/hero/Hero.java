8477409a5f1e74960eab728c28d929038c149215
Crabi-Remery <romain.dugas85@gmail.com>
2023-11-21 13:41:13 +0100

    Ajout des tests unitaires du package item

--- src/hero/Hero.java
+++ src/hero/Hero.java
@@ -19,7 +19,7 @@ public class Hero {
 	private static final int MULTIPLIER_LEVEL = 2;
 	private static final double CONSTANT_PROGRESSION = 1.2;
 	private static Hero hero;
-	
+
 	//attribute
 	private final String NAME;
 	private Location loc;
@@ -33,35 +33,35 @@ public class Hero {
 	private int xp;
 	private int level;
 	private int xpNewLvl;
-	
+
 	private Hero(String name, Location loc, int hp, int mp, int xp, int level) {
 		this.NAME = name;
 		this.loc = loc;
-		
+
 		this.hp = hp;
 		this.maxHp = hp;
-		
+
 		this.mp = mp;
 		this.maxMp = mp;
-		
+
 		this.speed = Hero.DEFAULT_SPEED;
 		this.xp = xp;
 		this.level = level;
 		this.xpNewLvl = 10;
 		this.inventory = new Inventory();
 	}
-	
+
 	private Hero(String name, Location loc) {
 		this(name, loc, Hero.DEFAULT_HP, Hero.DEFAULT_MP, Hero.DEFAULT_XP, Hero.DEFAULT_LEVEL);
 	}
-	
+
 	public static Hero createHero(String name, Location loc) {
 		if (Hero.hero == null) {
 			Hero.hero = new Hero(name, loc);
 		}
 		return Hero.hero;
 	}
-	
+
 	public static Hero getHero() throws Exception {
 		if (Hero.hero != null) {
 			return Hero.hero;
@@ -69,76 +69,76 @@ public class Hero {
 			throw new Exception("getHero : No hero has been created !");
 		}
 	}
-	
+
 	public static void destroyHero() {
 		Hero.hero = null;
 	}
-	
+
 	@Override
 	public String toString() {
-		return String.format("%s :\n hp %d\n lvl %d \n", this.NAME, this.hp, this.level); 
+		return String.format("%s :\n hp %d\n lvl %d \n", this.NAME, this.hp, this.level);
 	}
-	
+
 	/*------------------------------------------------------------------------------------
-	 * 
+	 *
 	 * Getters
-	 * 
+	 *
 	 ------------------------------------------------------------------------------------*/
-	
+
 	public Location getLocation() {
 		return this.loc;
 	}
-	
+
 	public Weapon getWeaponEquiped() {
 		return this.weapon;
 	}
-	
+
 	public int getHp() {
 		return this.hp;
 	}
-	
+
 	public int getMaxHp() {
 		return this.maxHp;
 	}
-	
+
 	public int getMp() {
 		return this.mp;
 	}
-	
+
 	public int getMaxMp() {
 		return this.maxMp;
 	}
-	
+
 	public int getSpeed() {
 		return this.speed;
 	}
-	
+
 	public int getLevel() {
 		return this.level;
 	}
-	
+
 	public int getXp() {
 		return this.xp;
 	}
-	
+
 	public int getXpNewLevel() {
 		return this.xpNewLvl;
 	}
-	
+
 	public int getDamage() {
 		return this.weapon.getDamage();
 	}
-	
+
 	public int getRange() {
 		return this.weapon.getRange();
 	}
-	
+
 	/*------------------------------------------------------------------------------------
-	 * 
+	 *
 	 * Setters
-	 * 
+	 *
 	 ------------------------------------------------------------------------------------*/
-	
+
 	public void setHp(int hp) {
 		this.hp = hp;
 		if (this.hp > this.maxHp) {
@@ -148,15 +148,15 @@ public class Hero {
 			this.hp = 0;
 		}
 	}
-	
+
 	public void heal(int hp) {
 		setHp(this.hp + hp);
 	}
-	
+
 	public void getAttacked(int damage) {
 		setHp(this.hp - damage);
 	}
-	
+
 	public void setMp(int mp) {
 		this.mp = mp;
 		if (this.mp > this.maxMp) {
@@ -166,81 +166,81 @@ public class Hero {
 			this.mp = 0;
 		}
 	}
-	
+
 	public void gainMp(int mp) {
 		setMp(this.mp + mp);
 	}
-	
+
 	public void useMp(int mp) {
 		setMp(this.mp - mp);
 	}
-	
+
 	private void changeHpLvl() {
 		this.maxHp *= CONSTANT_PROGRESSION;
 		this.hp *= CONSTANT_PROGRESSION;
 	}
-	
+
 	private void changeMpLvl() {
 		if (this.maxMp == 0) {
 			this.maxMp += 10;
 			this.mp += 10;
-		}	
+		}
 		else {
 			this.maxMp *= CONSTANT_PROGRESSION;
 			this.mp *= CONSTANT_PROGRESSION;
 		}
 	}
-	
+
 	public void addXp(int xp) {
 		this.xp += xp;
 		if (this.xpNewLvl <= this.xp) {
 			this.levelUp();
 		}
 	}
-	
+
 	public void levelUp() {
 		this.level++;
 		this.xp -= this.xpNewLvl;
 		//2^(level-1) x 10
 		this.xpNewLvl = (int)Math.pow(Hero.MULTIPLIER_LEVEL, this.level-1)*10;
-		
+
 		this.changeHpLvl();
-		this.changeMpLvl();	
-		
+		this.changeMpLvl();
+
 		this.addXp(0); //if there is another level to gain
-	}	
-	
-	
+	}
+
+
 	public void setLocation(Location loc) {
 		this.loc = loc;
 	}
-	
+
 	public void changeEquipedWeapon(Weapon weapon) {
 		this.weapon = weapon;
 	}
-	
+
 	public void addItem(Item item) throws Exception{
 		this.inventory.addItem(item);
 	}
-	
+
 	public void removeItem(Item item) throws Exception{
 		this.inventory.removeItem(item);
 	}
-	
+
 	/*------------------------------------------------------------------------------------
-	 * 
+	 *
 	 * Others
-	 * 
+	 *
 	 ------------------------------------------------------------------------------------*/
-	
+
 	public void useItem(Usable item) {
 		item.use();
 	}
-	
+
 	public void attack(Character character) {
 		character.getAttacked(hero.getWeaponEquiped().getDamage());
 	}
-	
+
 	public void printInventory() {
 		this.inventory.printInventory();
 	}
---
+++ src/tests/ItemTest.java
@@ -0,0 +1,78 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import hero.Hero;
+import items.*;
+
+public class ItemTest {
+    private Hero testHero;
+
+    @Before
+    public void setUp() {
+        // Initialisation du héros pour les tests
+        testHero = Hero.createHero("Test", null);
+    }
+
+    @Test
+    public void testBow() {
+        Bow bow = new Bow();
+        assertEquals(5, bow.getWeight());
+        assertEquals(5, bow.getDamage());
+        assertEquals(10, bow.getRange());
+    }
+
+    @Test
+    public void testHealPotion() {
+        HealPotion healPotion = new HealPotion();
+        assertEquals(1, healPotion.getWeight()); // Potion a le poids correct
+        testHero.setHp(2);
+        healPotion.setHero(testHero);
+        healPotion.use();
+        assertEquals(7, testHero.getHp()); // Vérifiez si l'utilisation de la potion guérit le héros correctement
+    }
+
+    @Test
+    public void testMagicWand() {
+        MagicWand magicWand = new MagicWand();
+        assertEquals(5, magicWand.getWeight());
+        assertEquals(10, magicWand.getDamage());
+        assertEquals(10, magicWand.getRange());
+    }
+
+    @Test
+    public void testSword() {
+        Sword sword = new Sword();
+        assertEquals(10, sword.getWeight());
+        assertEquals(10, sword.getDamage());
+        assertEquals(10, sword.getRange());
+    }
+
+    @Test
+    public void testPotionWeight() {
+        Potion potion = new HealPotion();
+        assertEquals(1, potion.getWeight());
+    }
+
+    @Test
+    public void testWeaponWeight() {
+        Weapon weapon = new Bow();
+        assertEquals(5, weapon.getWeight());
+    }
+
+    @Test
+    public void testWeaponDamage() {
+        Weapon weapon = new Sword();
+        assertEquals(10, weapon.getDamage());
+    }
+
+    @Test
+    public void testWeaponRange() {
+        Weapon weapon = new MagicWand();
+        assertEquals(10, weapon.getRange());
+    }
+}
+
