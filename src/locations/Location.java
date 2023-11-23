444a1a9c767ceb4c8fcf02b1eb3721de57a0d962
PhiphiLeBoss12 <100560205+PhiphiLeBoss12@users.noreply.github.com>
2023-11-23 14:57:48 +0100

    Added locations creator

--- src/locations/Location.java
+++ src/locations/Location.java
@@ -4,12 +4,39 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-public abstract class Location {
+public class Location {
     private final String NAME;
     private final String DESCRIPTION;
 
     private HashMap<String, Exit> exits = new HashMap<String, Exit>();
     private List<Character> characters = new ArrayList<Character>();
+    
+    /**
+     * Returns a list of locations for the game
+     */
+    public static ArrayList<Location> createGameLocations() {
+        ArrayList<Location> locs = new ArrayList<Location>();
+
+        // TODO: Add characters to the locations, and mayber transfer them in the Game class
+        // Creation of the locations
+        locs.add(new Location("Le commencement", "Une île paisible loin de tout le reste du monde"));
+        locs.add(new Location("Île n°1", "Une île inconnue située à une extrémité de l'archipel. Vous pouvez observer une autre île au loin."));
+        locs.add(new Location("Île n°2", "L'île principale de l'archipel."));
+        locs.add(new Location("Île de la quête", "Une île perdue presque hors de l'archipel. L'ambiance y est hostile."));
+
+        // Exits for the 1st island
+        locs.get(1).addExit(locs.get(2), "L'île au loin semble atteignable en nageant grâce au courant.");
+        locs.get(1).addExit(locs.get(1), "Vous observez une grotte sur l'île, et des symboles semblent indiquer qu'un code est nécessaire afin d'y pénetrer");
+
+        // Exits for the 2nd island
+        locs.get(2).addExit(locs.get(3), "En observant l'horizon, vous distinguez aisément une immense île. Elle semble habitée. S'y rendre à la nage semble possible");
+        
+        // Exits for the 3rd island
+        locs.get(3).addExit(locs.get(1), "Vous semblez pouvoir vous rendre à la première île visitée à la nage.");
+        locs.get(3).addExit(locs.get(2), "L'île principale semble aussi atteignable aisément.");       
+
+        return locs;
+    }
 
 
     /* --------------------*
@@ -32,8 +59,15 @@ public abstract class Location {
     /**
      * Adds an exit to the location
      */
-    public void addExit(String name, Exit exit) {
-        this.exits.put(name, exit);
+    public void addExit(Location loc, String description) {
+        Exit exit = new Exit(loc, description);
+        this.exits.put(loc.getName(), exit);
+    }
+
+    // TODO: ???
+    public void addExitWithCode(Location loc, String code, String description) {
+        Exit exit = new ExitWithCode(loc, code, description);
+        this.exits.put(loc.getName(), exit);
     }
 
     /**
