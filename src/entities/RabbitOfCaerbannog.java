045cb6b3a2a3de45e244dfaddaf309e7340c71df
Crabi-Remery <romain.dugas85@gmail.com>
2023-11-30 22:11:36 +0100

    Changement du nom du RABBIT

--- src/entities/RabbitOfCaerbannog.java
+++ src/entities/RabbitOfCaerbannog.java
@@ -14,7 +14,7 @@ public class RabbitOfCaerbannog extends Fighter {
 	private static final int XP_DROPPED = 1000;
 
 	public RabbitOfCaerbannog() {
-		super("Rabbit Of Caerbannog", 1, Fighter.MAX_DAMAGE, RabbitOfCaerbannog.XP_DROPPED, Fighter.MAX_SPEED);
+		super("RabbitOfCaerbannog", 1, Fighter.MAX_DAMAGE, RabbitOfCaerbannog.XP_DROPPED, Fighter.MAX_SPEED);
 		this.setDescription("A cute rabbit surrounded by corpses.");
 	}
 }
